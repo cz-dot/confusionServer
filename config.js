c63fc734df6fae238c9fdd1fb3164b02063ff014
格莱 <zhouchang29@gmail.com>
2019-06-30 18:50:05 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '60928-94594-22358-45340',
-  'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+  'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: 'Your Client App ID',
+    clientSecret: 'Your Client App Secret'
+  }
 }
